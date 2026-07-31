f9f7449799a56a26fa3e95e1eba105212b921585
Breezy <923803814@qq.com>
2026-08-01 01:54:27 +0800

    fix: defer control plane database cleanup

--- services/control-plane/cmd/harness-forge/main.go
+++ services/control-plane/cmd/harness-forge/main.go
@@ -20,14 +20,13 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer pool.Close()
 	if err := postgres.Migrate(context.Background(), pool, "public"); err != nil {
-		pool.Close()
 		log.Fatal(err)
 	}
 
 	log.Printf("control plane listening on %s", applicationConfig.HTTPAddr)
 	err = http.ListenAndServe(applicationConfig.HTTPAddr, httpapi.NewRouter())
-	pool.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
